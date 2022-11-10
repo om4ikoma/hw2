6a7ca4d6da6d833adfd6a5dd3d156af9bd8b4b2e
skoddrri <heavystarting@gmail.com>
2022-11-10 17:55:25 +0600

    hw8

--- main/admin.py
+++ main/admin.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from main.models import Film, Director
 
 
-class Films(admin.ModelAdmin):
-    list_display = ['name']
-    search_fields = 'id title text'.split()
-    list_per_page = 3
+class FilmAdmin(admin.ModelAdmin):
+    list_display = ['director', 'title', 'rating', 'duration', ]
+    search_fields = 'title'.split()
+    list_filter = ['director', 'title', 'rating', 'duration', ]
+    list_display_links = None
+    list_editable = 'director title rating'.split()
 
 
-admin.site.register(Film)
+admin.site.register(Film, FilmAdmin)
 admin.site.register(Director)
-
--- main/models.py
+++ main/models.py
@@ -24,10 +24,10 @@ class Film(models.Model):
     class Meta:
         verbose_name = 'Фильмы'
         verbose_name_plural = 'Фильмы'
-    title = models.CharField(max_length=100, null=True)
-    rating = models.IntegerField(null=True, choices=STARS)
-    duration = models.IntegerField(null=True)
-    director = models.ForeignKey(Director, null=True, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, null=True, verbose_name='Название:')
+    rating = models.IntegerField(null=True, choices=STARS, verbose_name='Рейтинг:')
+    duration = models.IntegerField(null=True, verbose_name='Длительность:')
+    director = models.ForeignKey(Director, null=True, on_delete=models.CASCADE, verbose_name='Директор:')
 
     def __str__(self):
         return self.title
